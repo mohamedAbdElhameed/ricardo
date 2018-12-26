f40daf70bcb9050b4382e74083c10aa8b4dae895
MohamedAbdElhameed <mohamedabdelhameed34@gmail.com>
2018-12-26 21:13:13 +0200

    values ()

--- products/apis/views.py
+++ products/apis/views.py
@@ -177,7 +177,7 @@ class CartViewForMobile(RetrieveAPIView):
             form = form.replace('{{action_url}}', "https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/")
             small_carts.append({
                 'seller': seller.name,
-                'data': products,
+                'data': products.values(),
                 'payment_form': form,
                 # 'APIKEY': apikey,
                 # 'merchant_id': merchant_id,
